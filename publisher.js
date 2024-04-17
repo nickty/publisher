b137f3bb8d771ea33c53c9d5a12603f7e3ef3139
Mizanur Rahman <mizan@arogga.net>
2024-04-17 11:56:06 +0600

    publisher initial

---
+++ publisher.js
@@ -0,0 +1,20 @@
+// publisher.js
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://localhost:1883");
+
+client.on("connect", function () {
+  setInterval(function () {
+    const messageObject = {
+      time: new Date().toString(),
+      message: "Hello MQTT with JSON",
+      arr: ["he", "you", "me"],
+    };
+    const message = JSON.stringify(messageObject);
+    client.publish("test/topic", message);
+    console.log("Message Sent:", message);
+  }, 5000); // Publish every 5 seconds
+});
+
+client.on("error", function (error) {
+  console.log("MQTT Client Error:", error);
+});
